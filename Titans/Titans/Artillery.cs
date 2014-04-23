68fdd2b6b95de3c75170425edb660d0e3d501704
bobsnowdog <bobsnowdog@yahoo.com>
2014-04-22 19:01:13 -0500

    trial comment
    blah blah blah blah

--- Titans/Titans/Artillery.cs
+++ Titans/Titans/Artillery.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Titans
 {
+    //Created the artillery unit
     class Artillery : Unit
     {
         public override int HP { get; set; }
